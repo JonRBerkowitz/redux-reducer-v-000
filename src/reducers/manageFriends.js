2e166e1055b8910545cb7a6422d74774c6dfec8e
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-04-16 21:00:17 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case expression: "ADD_FRIEND"
+      return { ...friends: state.friends + action.friend }
+    default: return state;
+
+  }
 }
